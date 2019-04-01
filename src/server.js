1ac79ec655474317563b5d85cfdd9cafe39d22a9
Maksim Korzh <freesoft.for.people@gmail.com>
2019-04-01 17:07:20 +0300

    created tutorial

---
+++ src/server.js
@@ -0,0 +1,13 @@
+// Import packages
+const express = require('express');
+const path = require('path');
+
+// create app instance
+const app = express();
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'docs')));
+
+// Set up port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
